5119123f7832460de61f70a86879f70380f56f48
mytte <mytte@10.0.0.115>
2024-04-21 16:05:20 -0600

    CSC320 Restaurant Name & Address Information Project. Option 2.

---
+++ RestaurantInformation/RestaurantInformation.java
@@ -0,0 +1,19 @@
+package RestaurantInfoPackage;
+
+public class RestaurantInformation {
+
+	public static void main(String[] args) {
+		
+		String restaurantName = "Noon Thai Plus Vegan";
+		String businessAddress = "3712 S Broadway";
+		String restaurantCity = "Englewood";
+		String restaurantState = "Colorado";
+		String restaurantZipcode = "80113";
+		
+		System.out.println("Restaurant Name: " + restaurantName);
+		System.out.println("Business Address: " + businessAddress);
+		System.out.println("City, State, and Zip Code: " + restaurantCity + ", " + restaurantState + ", " + restaurantZipcode);
+
+	}
+
+}
